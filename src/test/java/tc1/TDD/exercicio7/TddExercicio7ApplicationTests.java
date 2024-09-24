39997398878f0c3700f9cc02a23cbdd413ed0fd4
Adriann Postigo <adriannparanhos@gmail.com>
2024-09-24 19:31:52 -0300

    Test: add Annotation @DisplayName for testeFotosCabem and testeFotosNaoCabem and create test testeFotosNaoCabem

--- src/test/java/tc1/TDD/exercicio7/TddExercicio7ApplicationTests.java
+++ src/test/java/tc1/TDD/exercicio7/TddExercicio7ApplicationTests.java
@@ -1,13 +1,16 @@
 package tc1.TDD.exercicio7;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class TddExercicio7ApplicationTests {
 
 	@Test
+	@DisplayName("Should verify if two photos fit inside the album page for various dimensions")
 	public void testFotosCabem() {
 		AlbumPhotos albumPhotos = new AlbumPhotos();
 		assertTrue(albumPhotos.podeColarFotos(7, 5, 3, 4));
@@ -15,6 +18,17 @@ class TddExercicio7ApplicationTests {
 		assertTrue(albumPhotos.podeColarFotos(10, 10, 5, 4));
 		assertTrue(albumPhotos.podeColarFotos(6, 8, 3, 4));
 	}
+
+	@Test
+	@DisplayName("Should verify if photos do not fit inside the album page for various dimensions")
+	public void testFotosNaoCabem() {
+		AlbumPhotos albumPhotos = new AlbumPhotos();
+		assertFalse(albumPhotos.podeColarFotos(7, 5, 5, 5));
+		assertFalse(albumPhotos.podeColarFotos(10, 10, 6, 7));
+		assertFalse(albumPhotos.podeColarFotos(5, 5, 3, 4));
+		assertFalse(albumPhotos.podeColarFotos(5, 5, 5, 5));
+	}
+
 
 
 }
